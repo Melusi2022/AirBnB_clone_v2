b23df9d89a1830fb5c751744454cde2a7720541b
Melusi2022 <melusimedia@gmail.com>
2024-03-24 18:49:28 +0000

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Starts a flask app
+listens to 0.0.0.0 on port 5000
 """
-
+import os
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
+@app.route("/states", strict_slashes=False)
+def cities_by_states():
+    """
+    Displays list of all cities by State objects in DBStorage.
+    """
     states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """
+    Displays page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """Remove the current session"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
